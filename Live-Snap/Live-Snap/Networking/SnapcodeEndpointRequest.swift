4da781da2f59e9fb4969e1031497a93332990e36
Oleg Abalonski <Abalonski@gmail.com>
2018-01-06 22:15:54 -0500

    Changed start function to check request result

--- Live-Snap/Live-Snap/Networking/SnapcodeEndpointRequest.swift
+++ Live-Snap/Live-Snap/Networking/SnapcodeEndpointRequest.swift
@@ -17,22 +17,24 @@ class SnapcodeEndpointRequest {
         self.snapchatUsername = snapchatUsername
     }
     
-    //https://github.com/Alamofire/AlamofireImage
-    //Shouldn't we be Almofire.download the image???
-    // Make a network call to api and return a snapcode image if successful
     func start(completion: @escaping ((UIImage?) -> ())) {
         
         let url = "https://feelinsonice-hrd.appspot.com/web/deeplink/snapcode?username=\(snapchatUsername)&type=PNG&size=800"
     
-        // Make Alamofire request
-        Alamofire.request(url).responseData { response in
-            if let imageData = response.result.value {
-                let image = UIImage(data: imageData)
-                completion(image)
-            }
-        }
+        Alamofire.request(url).responseData { (response: DataResponse<Data>) in
 
-    
+            response.result.ifSuccess {
+                if let imageData = response.result.value {
+                    let image = UIImage(data: imageData)
+                    completion(image)
+            }
+            }
+            
+            response.result.ifFailure {
+                print("Error in making the request")
+            }
+                
+        
+        }
     }
-    
 }
