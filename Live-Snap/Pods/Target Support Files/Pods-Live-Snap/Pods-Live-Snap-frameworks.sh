409a75203f173e120a8981eb9cf41c854b92f0a9
Baby <doinggodswork365@gmail.com>
2018-02-08 18:53:50 -0800

    Remove GPUImage and add apple accelerate UIImage extension for blurring

--- Live-Snap/Pods/Target Support Files/Pods-Live-Snap/Pods-Live-Snap-frameworks.sh
+++ Live-Snap/Pods/Target Support Files/Pods-Live-Snap/Pods-Live-Snap-frameworks.sh
@@ -91,12 +91,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CropViewController/CropViewController.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/GPUImage/GPUImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CropViewController/CropViewController.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/GPUImage/GPUImage.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
