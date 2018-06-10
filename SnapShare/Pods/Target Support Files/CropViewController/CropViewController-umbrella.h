bb26d3084d455684417421de5491c84e2efb7102
Oleg Abalonski <o_abalonski@yahoo.com>
2018-06-10 02:28:46 -0400

    rename project/app

---
+++ SnapShare/Pods/Target Support Files/CropViewController/CropViewController-umbrella.h
@@ -0,0 +1,28 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CropViewController-Bridging-Header.h"
+#import "CropViewController.h"
+#import "TOActivityCroppedImageProvider.h"
+#import "TOCroppedImageAttributes.h"
+#import "TOCropViewControllerTransitioning.h"
+#import "UIImage+CropRotate.h"
+#import "TOCropViewController-Bridging-Header.h"
+#import "TOCropViewController.h"
+#import "TOCropOverlayView.h"
+#import "TOCropScrollView.h"
+#import "TOCropToolbar.h"
+#import "TOCropView.h"
+
+FOUNDATION_EXPORT double CropViewControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char CropViewControllerVersionString[];
+
